49c353d58310bd3d179493c35792821ebc0af4d9
maciel050708 <macielmonteirojm@gmail.com>
2020-03-05 07:51:15 +0900

    Correção função somar

---
+++ www/components/contar.js
@@ -0,0 +1,11 @@
+ $(document).on("click","#somar",function() {
+    var valor1 = $("#valor1").val();
+    var valor2 = $("#valor2").val();
+
+    var result = parseInt(valor1) + parseInt(valor2);
+
+    console.log("teste");
+    $("#result").val(result);
+  
+});
+
